606481f5b8df345ba4d530aa6e1b779f4d249b7d
AvaN0x <clement.ricatte57@gmail.com>
2021-02-17 10:37:46 +0100

    Edit ServeurMain to start the allumettes serveur

--- serveur/src/ServeurMain.java
+++ serveur/src/ServeurMain.java
@@ -1,8 +1,13 @@
 package serveur.src;
 
+import serveur.src.controleurs.allumettes.ServeurAllumettes;
+
 public class ServeurMain {
 
     public static void main(String[] args) {
-        System.out.println(">> The server may be ON, or maybe not");
+        System.out.println(">> Lancement du serveur principal.");
+        int port = 6000;
+        String hote = "localhost";
+        ServeurAllumettes.lancer(hote, port);
     }
 }
