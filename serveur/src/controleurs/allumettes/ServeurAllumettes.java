e147284f74c37047a830e8b24e06488d10dc4b75
AvaN0x <clement.ricatte57@gmail.com>
2021-02-17 10:37:22 +0100

    started working on the Allumettes game

---
+++ serveur/src/controleurs/allumettes/ServeurAllumettes.java
@@ -0,0 +1,20 @@
+package serveur.src.controleurs.allumettes;
+
+import java.rmi.*;
+import java.rmi.registry.*;
+
+import serveur.src.modeles.allumettes.*;
+
+public class ServeurAllumettes {
+    public static void lancer(String hote, int port) {
+        try {
+            LocateRegistry.createRegistry(port);
+
+            Naming.rebind("rmi://" + hote + ":" + port + "/banque", new AllumettesJeux());
+
+            System.out.println(">> Serveur Allumettes prêt !");
+        } catch (Exception e) {
+            System.out.println(">> Echec de connexion au serveur bancaire : " + e);
+        }
+    }
+}
---
+++ serveur/src/modeles/allumettes/IAllumettesJeux.java
@@ -0,0 +1,13 @@
+package serveur.src.modeles.allumettes;
+
+import java.rmi.*;
+
+public interface IAllumettesJeux extends Remote {
+    public void lancerPartie() throws RemoteException;
+
+    public void retirer(int quantite) throws RemoteException;
+
+    public int getAllumettesRestantes() throws RemoteException;
+
+    public void setAllumettesRestantes(int allumettesRestantes) throws RemoteException;
+}
