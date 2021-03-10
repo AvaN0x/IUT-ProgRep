b4d3d165ca37707a8cdad5f876a5de0366674623
AvaN0x <clement.ricatte57@gmail.com>
2021-03-10 08:32:40 +0100

    we can now start multiple servers

--- serveur/src/controleurs/allumettes/ServeurAllumettes.java
+++ serveur/src/controleurs/allumettes/ServeurAllumettes.java
@@ -7,9 +7,14 @@ import serveur.src.modeles.allumettes.*;
 
 public class ServeurAllumettes {
     public ServeurAllumettes(String hote, int port) {
+        // ? Essayer de créer le registre, s'il est déjà crée une erreur aura lieu, mais
+        // ? le serveur sera toujours configuré
+        // ? Cette exception arrive si un autre serveur a déjà été lancé avant
         try {
             LocateRegistry.createRegistry(port);
-
+        } catch (RemoteException e) {
+        }
+        try {
             Naming.rebind("rmi://" + hote + ":" + port + "/allumettes", new Allumettes());
 
             System.out.println(">> Serveur Allumettes prêt !");
--- serveur/src/controleurs/pendu/ServeurPendu.java
+++ serveur/src/controleurs/pendu/ServeurPendu.java
@@ -7,9 +7,14 @@ import serveur.src.modeles.pendu.*;
 
 public class ServeurPendu {
     public ServeurPendu(String hote, int port) {
+        // ? Essayer de créer le registre, s'il est déjà crée une erreur aura lieu, mais
+        // ? le serveur sera toujours configuré
+        // ? Cette exception arrive si un autre serveur a déjà été lancé avant
         try {
             LocateRegistry.createRegistry(port);
-
+        } catch (RemoteException e) {
+        }
+        try {
             Naming.rebind("rmi://" + hote + ":" + port + "/pendu", new Pendu());
 
             System.out.println(">> Serveur Pendu prêt !");
